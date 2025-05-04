2c58373dfa666ab3a424de8d238c77790e67240b
Ayush201738982361 <ayushnair87@gmail.com>
2025-05-04 23:02:02 +0530

    Implemented changing of page based on selected tab's state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,17 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Sidebar from "./components/Sidebar";
 import CreatePost from "./components/CreatePost";
+import PostList from "./components/PostList";
+import { useState } from "react";
 
 function App() {
+  const [selectedTab, setSelectedTab] = useState("Home");
   return (
     <div className="app-container">
-      <Sidebar />
+      <Sidebar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
       <div className="content">
         <Navbar />
-        <CreatePost />
+        {selectedTab === "Home" ? <PostList /> : <CreatePost />}
         <Footer />
       </div>
     </div>
