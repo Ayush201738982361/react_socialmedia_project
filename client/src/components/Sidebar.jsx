6c40b57e4393e87eaf9a8cfee585dd2e29bedee8
Ayush201738982361 <ayushnair87@gmail.com>
2025-05-04 23:00:03 +0530

    Added states for toggling between tabs

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function Sidebar() {
+export default function Sidebar({ selectedTab, setSelectedTab }) {
   return (
     <div
       className="d-flex flex-column flex-shrink-0 p-3 text-bg-dark sidebar"
@@ -22,8 +22,19 @@ export default function Sidebar() {
       </a>
       <hr />
       <ul className="nav nav-pills flex-column mb-auto">
-        <li className="nav-item">
-          <a href="#" className="nav-link active" aria-current="page">
+        <li
+          className="nav-item"
+          onClick={() => {
+            setSelectedTab("Home");
+          }}
+        >
+          <a
+            href="#"
+            className={`nav-link text-white ${
+              selectedTab === "Home" && "active"
+            }`}
+            aria-current="page"
+          >
             <svg
               className="bi pe-none me-2"
               width="16"
@@ -35,8 +46,17 @@ export default function Sidebar() {
             Home
           </a>
         </li>
-        <li>
-          <a href="#" className="nav-link text-white">
+        <li
+          onClick={() => {
+            setSelectedTab("Create Post");
+          }}
+        >
+          <a
+            href="#"
+            className={`nav-link text-white ${
+              selectedTab === "Create Post" && "active"
+            }`}
+          >
             <svg
               className="bi pe-none me-2"
               width="16"
