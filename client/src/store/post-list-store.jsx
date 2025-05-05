fa87e3a626b2fbe0bde7173a7154a372f2be75ee
Ayush201738982361 <ayushnair87@gmail.com>
2025-05-05 23:10:11 +0530

    Created context for adding and deleting post

---
+++ client/src/store/post-list-store.jsx
@@ -0,0 +1,13 @@
+import { createContext } from "react";
+
+const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
+
+const PostListProvider = ({ children }) => {
+  return <PostList.Provider value={[]}>{children}</PostList.Provider>;
+};
+
+export default PostListProvider;
