ec22a0f42ed473a127811e855c64689f6a0f5338
Ayush201738982361 <ayushnair87@gmail.com>
2025-05-06 23:17:10 +0530

    Provided default dummy values to reducer to render

--- client/src/store/post-list-store.jsx
+++ client/src/store/post-list-store.jsx
@@ -1,13 +1,48 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 
-const PostList = createContext({
+export const PostList = createContext({
   postList: [],
   addPost: () => {},
   deletePost: () => {},
 });
 
-const PostListProvider = ({ children }) => {
-  return <PostList.Provider value={[]}>{children}</PostList.Provider>;
+const postListReducer = (currPostList, action) => {
+  return currPostList;
 };
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
+
+  const addPost = () => {};
+  const deletePost = () => {};
+
+  return (
+    <PostList.Provider value={{ postList, addPost, deletePost }}>
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POST_LIST = [
+  {
+    id: 1,
+    title: "Title",
+    body: "This is the body",
+    reactions: 0,
+    userId: 9,
+    tags: ["HelloWorld"],
+  },
+  {
+    id: 2,
+    title: "Titl2",
+    body: "This is the body of content 2",
+    reactions: 0,
+    userId: 9,
+    tags: ["HelloWorld1232"],
+  },
+];
 
 export default PostListProvider;
