d817b6f0dd57c84872d503627c610d1e8b957c52
Ayush201738982361 <ayushnair87@gmail.com>
2025-05-12 12:20:14 +0530

    Added cleaner function for useEffect hook

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,26 +1,33 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import WelcomeMessage from "./WelcomeMessage";
 import { PostList as PostListData } from "../store/post-list-store";
 import Post from "./Post";
+import LoadingSpinner from "./LoadingSpinner";
 
 function PostList() {
   const { postList, addInitialPost } = useContext(PostListData);
+  const [fetching, setFetching] = useState(false);
 
   useEffect(() => {
+    setFetching(true);
     fetch("https://dummyjson.com/posts")
       .then((res) => res.json())
       .then((data) => {
+        setFetching(false);
         addInitialPost(data.posts);
-      }),
-      [];
-  });
+      });
+    return () => {
+      console.log("Cleaning Up useEffect");
+    };
+  }, []);
 
   return (
     <>
-      {postList.length === 0 ? (
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 ? (
         <WelcomeMessage />
       ) : (
-        postList.map((post) => <Post key={post.id} post={post} />)
+        !fetching && postList.map((post) => <Post key={post.id} post={post} />)
       )}
     </>
   );
