f7459e00117d7062654148ded46d82d7eb8283a0
Ayush201738982361 <ayushnair87@gmail.com>
2025-05-12 12:17:32 +0530

    Changed Loading Spinner

---
+++ client/src/components/LoadingSpinner.jsx
@@ -0,0 +1,13 @@
+function LoadingSpinner() {
+  return (
+    <center>
+      <div
+        className="spinner-border m-5 spinner"
+        style={{ width: "4rem", height: "4rem" }}
+        role="status"
+      ></div>
+    </center>
+  );
+}
+
+export default LoadingSpinner;
