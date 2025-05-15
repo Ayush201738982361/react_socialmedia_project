a4954f907bded4d5ed4f3a4684031f548c36251f
Ayush201738982361 <ayushnair87@gmail.com>
2025-05-15 13:09:43 +0530

    Moved fetch to context

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,29 +1,11 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext } from "react";
 import WelcomeMessage from "./WelcomeMessage";
 import { PostList as PostListData } from "../store/post-list-store";
 import Post from "./Post";
 import LoadingSpinner from "./LoadingSpinner";
 
 function PostList() {
-  const { postList, addInitialPost } = useContext(PostListData);
-  const [fetching, setFetching] = useState(false);
-
-  const controller = new AbortController();
-  const _signal = controller.signal;
-
-  useEffect(() => {
-    setFetching(true);
-    fetch("https://dummyjson.com/posts")
-      .then((res) => res.json())
-      .then((data) => {
-        setFetching(false);
-        addInitialPost(data.posts);
-      });
-    return () => {
-      console.log("Cleaning Up useEffect");
-      controller.abort();
-    };
-  }, []);
+  const { postList, fetching } = useContext(PostListData);
 
   return (
     <>
